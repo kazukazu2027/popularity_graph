dc38565edfd4e192ca640eebdfd7614fe09f8007
kazukazu2027 <kazu2000027@gmail.com>
2021-10-22 19:01:27 +0900

    make prefectureNames

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header/Header';
 import Layout from './Layout/Layout';
+import PrefectureNames from './PrefectureNames/PrefectureNames';
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Layout>a</Layout>
+      <Layout>
+        <PrefectureNames />
+      </Layout>
     </div>
   );
 }
