4559be74aa16b975b5c294072efbbaf3df082375
kazukazu2027 <kazu2000027@gmail.com>
2021-10-22 18:39:46 +0900

    add_layout_component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
+import Layout from './Layout/Layout';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Layout>a</Layout>
     </div>
   );
 }
